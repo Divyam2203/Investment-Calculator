5fb130bcfb7f02d63928a4cf4c48b6bd6ebe98a5
Divyam <divyam2203@gmail.com>
2024-06-11 00:52:51 +0530

    app ready to deploy :)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,9 @@
+import {logo} from "src/assets/investment-calculator-logo.png"
+
 export default function Header(){
   return(
     <div id="header">
-      <img src="src\assets\investment-calculator-logo.png" alt="investment calculator logo" />
+      <img src={logo} alt="investment calculator logo" />
     <h1>Investment Calculator</h1>
     </div>
   );
